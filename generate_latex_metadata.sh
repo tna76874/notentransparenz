6faa5dc510780f41cba52fee278107f966d2d4ff
lmh <dev@hilberg.eu>
2024-07-07 07:40:15 +0200

    add commit hash to pdf

--- generate_latex_metadata.sh
+++ generate_latex_metadata.sh
@@ -5,4 +5,5 @@ timestamp=$(git log -1 --format=%cd --date=format-local:%d.%m.%Y ${TEX_FILE})
 echo "$timestamp" > ${TEX_DIR}/changed.tex
 
 COMMIT=$(git log -1 --pretty=format:"%H" ${TEX_FILE})
-echo "\href{https://github.com/tna76874/notentransparenz/tree/${COMMIT}}{Stand}" > ${TEX_DIR}/commit.tex
+echo "\href{https://github.com/tna76874/notentransparenz/tree/${COMMIT}}{Version ${COMMIT}}" > ${TEX_DIR}/commit_github.tex
+echo "${COMMIT}" > ${TEX_DIR}/commit.tex
