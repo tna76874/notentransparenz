720131d31649c931caceca3fb81ad6887459c338
lmh <dev@hilberg.eu>
2024-07-07 09:38:25 +0200

    update pandoc markdown conversion

--- generate_markdown_from_latex.sh
+++ generate_markdown_from_latex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 TEX_DIR='docs/files'
 TEX_FILE=${TEX_DIR}/notentransparenz.tex
+REF_FILE=${TEX_DIR}/references.bib
+METADATA_FILE=${TEX_DIR}/metadata.yml 
 
-pandoc -s "${TEX_FILE}" -o "${TEX_DIR}/notentransparenz.md" --mathjax
+pandoc -s "${TEX_FILE}" -o "${TEX_DIR}/notentransparenz.md" --katex --bibliography="${REF_FILE}" --metadata-file="${METADATA_FILE}"
