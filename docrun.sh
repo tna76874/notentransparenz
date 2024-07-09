e9ae0280b6af2d89a3b354b031e5474439fee450
lmh <dev@hilberg.eu>
2024-07-09 14:32:10 +0200

    add version to pip install

--- docrun.sh
+++ docrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export build_doc_date=$(date -u '+%d.%m.%Y')
-export commit_repo=$(git log -1 --pretty=format:"%H")
+export REPO_COMMIT=$(git log -1 --pretty=format:"%H")
 
 ./generate_latex_metadata.sh
 ./generate_markdown_from_latex.sh
 
 
-mike deploy --update-aliases ${commit_repo} latest
+mike deploy --update-aliases ${REPO_COMMIT} latest
 mike set-default latest
 mike serve
