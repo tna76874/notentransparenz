0fb3f7106c40f4f326a59d2b8226b98b09081cee
lmh <dev@hilberg.eu>
2024-07-09 13:04:24 +0200

    fix mike depoyment

--- docrun.sh
+++ docrun.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 export build_doc_date=$(date -u '+%d.%m.%Y')
+export commit_repo=$(git log -1 --pretty=format:"%H")
 
 ./generate_latex_metadata.sh
 ./generate_markdown_from_latex.sh
 
-mkdocs serve
+
+mike deploy --update-aliases ${commit_repo} latest
+mike set-default latest
+mike serve
